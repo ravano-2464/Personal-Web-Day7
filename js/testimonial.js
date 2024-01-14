9b42863bddba0c1f04a929888f4dff446a7aaf75
Ravano Akbar Widodo <118270814+ravano-2464@users.noreply.github.com>
2024-01-14 12:19:12 +0700

    Bootcamp Personal Web Day 7

--- js/testimonial.js
+++ js/testimonial.js
@@ -38,11 +38,11 @@ const testimonial1 = new Testimonial("Bayu", "Saya bangga dengan Chelsea", "http
 const testimonial2 = new Testimonial("Hima", "Saya bangga dengan City", "https://images.pexels.com/photos/3468827/pexels-photo-3468827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
 const testimonial3 = new Testimonial("Tayo", "Saya malu dengan MU", "https://images.pexels.com/photos/936019/pexels-photo-936019.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
 
-const testimonials = [testimonial1, testimonial2, testimonial3]
+const testimonial = [testimonial1, testimonial2, testimonial3]
 
 let testimonialHTML = ``
-for(let index = 0; index < testimonials.length; index++) {
-    testimonialHTML += testimonials[index].html()
+for(let index = 0; index < testimonial.length; index++) {
+    testimonialHTML += testimonial[index].html()
 }
 
-document.getElementById("testimonials").innerHTML = testimonialHTML
+document.getElementById("testimonial").innerHTML = testimonialHTML
